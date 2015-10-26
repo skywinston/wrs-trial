41fcdfeb72913be0b69f836bf536046ef730e83f
Sky Winston <skywinston@gmail.com>
2015-10-25 21:33:06 -0600

    Added support for form fadeIn on click of 'Try Cloud Stream For Free'

--- standard/script.js
+++ standard/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
+  $('.signup-form').hide();
 
+  $('.try').click(function(e){
+    e.preventDefault();
+
+    $(this).fadeOut();
+    $('.hero > .hero-text > h1').fadeOut();
+    $('.hero-byline').fadeOut(function(){
+      $('.signup-form').fadeIn();
+    });
+
+  });
   
-
 });
